519ab762332a2d931a125a1abd0e93c60d052c18
Raghunath T <traghunath98@hotmail.com>
2015-12-29 11:19:30 +0530

    Stabilising basic operations for single linked list.

--- JavaDataStructures/NodeList.java
+++ JavaDataStructures/NodeList.java
@@ -19,7 +19,10 @@ public class NodeList {
 
     public void addToHead(int el){ // add an element as the head
         head = new Node(el, head);
-        if (tail == null) tail = head;
+        if (tail == null) {
+            tail = head;
+            head.next = null;
+        }
     }
     
     public void addToTail(int el){ // add an element to the tail
@@ -81,13 +84,11 @@ public class NodeList {
         Node tmp;
         
         if(!isEmpty()){
-            tmp = head;
-            do{
+            
+            for(tmp = head; tmp.next != null; tmp = tmp.next){
                 str.append(tmp.info);
                 str.append(",");
-                tmp = tmp.next;
-            } while( tmp != tail);
-            
+            }
             str.append(tail.info);
             str.append(".");
             System.out.println("The String is: " + str.toString());
@@ -98,10 +99,45 @@ public class NodeList {
     
     public boolean isInList(int el){
         
-        return true;
+        boolean result = false;
+        
+        for(Node tmp = head; tmp != null; tmp = tmp.next){
+            if(tmp.info == el){
+                result = true;
+            }
+        }
+        return(result);
+        
     }
     
     public void delete(int el){
-        System.out.println ("Delete Method");
+        
+        Node tmp, prev;
+        prev = head;
+        for (tmp = head; tmp != null; tmp = tmp.next){
+         
+            if(tmp.info == el){ //tmp is the element to be removed.
+                
+                if((tmp == head) && !(tmp == tail)){ //list has more than one element. Remove the first one as it matches
+                    head = prev.next = tmp.next;
+                } else if((tmp == head) && (tmp == tail)){
+
+                    head = tail = null; //list has only one element and remove it.
+                    
+                } else if((tmp != head) && (tmp == tail)) { //list has many elements, but tail matches and hence it has to be removed
+                    prev.next = null;
+                    tail = prev;
+
+                } else { // The matched element is in the middle
+                    prev.next = tmp.next;
+                    tmp = null;
+                }
+                break;
+            }
+            if (!(tmp == head)){ // prev has to be one behind tmp always
+                prev = prev.next;
+            }
+            
+        }
     }
 }
--- JavaDataStructures/TestNode.java
+++ JavaDataStructures/TestNode.java
@@ -12,6 +12,26 @@ public class TestNode{
         }
         
         nList.printAll();
+        
+        System.out.println("Is element 79 in the list? " + nList.isInList(79));
+        
+        nList.delete(25);
+        nList.printAll();
+        
+        nList.delete(85);
+        nList.printAll();
+        
+        nList.delete(96);
+        nList.printAll();
+        
+        Node nj_new  = new Node(38);
+        NodeList nList_new = new NodeList();
+        nList_new.addToHead(nj_new.info);
+        
+        nList_new.printAll();
+        
+        nList_new.delete(38);
+        nList_new.printAll();
     }
 
 }
